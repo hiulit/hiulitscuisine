04bca219cbc17153d8cb745e399d6a7a97b96f45
hiulit <hiulit@gmail.com>
2018-07-09 16:00:42 +0200

    Fixed slugify function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,15 @@
 const ajax = new XMLHttpRequest()
 
 function slugify(str) {
-    const a = 'àáäâèéëêìíïîòóöôùúüûñçßÿœæŕśńṕẃǵǹḿǘẍźḧ·/_,:;'
-    const b = 'aaaaeeeeiiiioooouuuuncsyoarsnpwgnmuxzh------'
+    const a = 'àáäâèéëêìíïîòóöôùúüûñçñßÿœæŕśńṕẃǵǹḿǘẍźḧ·/_,:;'
+    const b = 'aaaaeeeeiiiioooouuuuncnsyoarsnpwgnmuxzh------'
     const p = new RegExp(a.split('').join('|'), 'g')
 
     return str.toString().toLowerCase()
         .replace(/\s+/g, '-')           // Replace spaces with -
         .replace(p, c =>
             b.charAt(a.indexOf(c)))     // Replace special chars
-        .replace(/&/g, '-and-')         // Replace & with 'and'
+        .replace(/&/g, '-i-')           // Replace & with 'i' (or the character/s in your language that means 'and')
         .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
         .replace(/\-\-+/g, '-')         // Replace multiple - with single -
         .replace(/^-+/, '')             // Trim - from start of text
